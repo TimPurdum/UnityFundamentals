06e03ad57901ed455cef96614d687dd878358d7b
Tim Purdum <tim@cedarrivertech.com>
2019-02-21 20:51:24 -0600

    stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,20 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     //private Animator anim;
-    private NavMeshAgent agent;
+    private NavMeshAgent _agent;
+    Animator _animator;
+    static readonly int _speed = Animator.StringToHash("Speed");
 
 
     // Start is called before the first frame update
     void Awake()
     {
-        //anim = GetComponent<Animator>();    
-        agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();    
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        _animator.SetFloat(_speed, _agent.velocity.magnitude);
     }
 }
